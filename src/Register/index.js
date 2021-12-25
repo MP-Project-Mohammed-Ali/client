23885aff15659f85bb0e2a950c828efd437826cd
M0hammed-18 <Mohammed.Alweheid.elq.js@tuwaiq.edu.sa>
2021-12-25 15:45:53 +0300

    SomeUpdate1

--- src/Register/index.js
+++ src/Register/index.js
@@ -7,6 +7,7 @@ export default function Register() {
 const navigate=useNavigate();
 const [name,setName]=useState('')
 const [email,setEmail]=useState('')
+const [password,setPassword]=useState('')
 
 
     return (
