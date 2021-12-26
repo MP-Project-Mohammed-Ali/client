1a7855eee0ce7c330cf25e5e98da95cfe04936d3
M0hammed-18 <Mohammed.Alweheid.elq.js@tuwaiq.edu.sa>
2021-12-26 09:12:37 +0300

    RegisterComponent

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
+import ReactDOM from "react-dom";
 import App from "./App";
-import store from "./reducers";
-
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById("root")
 );
+
